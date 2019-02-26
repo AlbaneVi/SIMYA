ef6c6adb60c80474c090ad5de6790494ee2c9f0c
Albane Villard <albane.villard@gmail.com>
2019-02-26 11:39:07 +0100

    add methods

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,2 +1,43 @@
 class EventsController < ApplicationController
+
+before_action :set_event, only: %i[edit update destroy]
+
+  def new
+    @event = Event.new
+  end
+
+  def edit
+  end
+
+  def update
+    @event.update(events_params)
+    redirect_to event_path
+  end
+
+  def create
+    @event = Event.new(events_params)
+    @event.user = current_user
+    if @event.save!
+      redirect_to event_path
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @event.destroy
+    redirect_to event_path
+  end
+
+  private
+
+  def set_events
+    @event = Event.find(params[:id])
+  end
+
+  def events_params
+    params.require(:event).permit(:title, :text, :date_start, :date_end)
+  end
+end
+
 end
