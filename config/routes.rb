f0053a2167539b02fbed20369ee9ee9903127654
emna <emna.dridi29@gmail.com>
2019-02-26 13:33:15 +0100

    view event show new edit

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :residencies
   resources :requests
-  resources :events, only: [:index, :show]
+  resources :events, only: [:index, :show, :new, :edit]
 end
