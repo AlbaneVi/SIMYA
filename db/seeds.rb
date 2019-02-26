71723d77aa1dd959235c85116bfc0c34a0b8e4c6
Albane Villard <albane.villard@gmail.com>
2019-02-26 13:42:58 +0100

    event show

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+User.destroy_all
+Event.destroy_all
+
+event1 = Event.new(title: "super journée",
+                    text:" blma blaiqmoigh^ù")
+event1.save!
+
+user1 = User.new(email:"toto@gmail.com",
+                  password:"toto@gmail.com"
+                  )
+user1.save!
+
