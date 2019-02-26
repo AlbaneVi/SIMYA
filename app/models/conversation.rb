9287107c16953abd6b6a0b1bbbfbce07d3b9991c
chloe ahkoon <ahkoon.c@gmail.com>
2019-02-26 16:32:09 +0100

    requests view

--- app/models/conversation.rb
+++ app/models/conversation.rb
@@ -4,6 +4,7 @@ class Conversation < ApplicationRecord
   has_many :requests, dependent: :destroy
 
   validates_uniqueness_of :sender_id, scope: :receiver_id
+
   scope :between, lambda { |sender_id, receiver_id|
     where("(conversations.sender_id = ? AND conversations.receiver_id = ?
           OR (conversations.receiver_id = ? AND conversations.sender_id = ?)",
