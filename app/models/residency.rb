0807a6b4f5da7d0617293f12a6bd006d3c5c501b
Albane Villard <albane.villard@gmail.com>
2019-02-27 16:16:48 +0100

    planning mensuel

--- app/models/residency.rb
+++ app/models/residency.rb
@@ -1,4 +1,12 @@
 class Residency < ApplicationRecord
   belongs_to :user
   belongs_to :transition
+
+  def start_time
+    self.date_start
+  end
+
+  def end_time
+    self.date_end - 1.day
+  end
 end
