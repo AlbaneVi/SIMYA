0a367f9566232f38559e45e56dad528537dd46b1
AlbaneVi <albane.villard@gmail.com>
2019-02-28 12:32:44 +0100

    fixed controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,9 @@
 Rails.application.routes.draw do
-  get 'requests/index'
-  get 'requests/create'
+  # get 'requests/index'
+  # get 'requests/create'
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :residencies
-  resources :events
-  resources :conversations, only: [:index, :create] do
-    resources :requests, only: [:index, :create]
-  end
+  resources :custodies, only: [:index, :show, :new, :create, :edit, :update]
+  resources :media, only: [:index, :show, :new, :create, :destroy]
 end
