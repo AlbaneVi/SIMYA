7955cfcb5d3e5856ddf33d5ecad4e2eed49c1dca
ahkoon <gAngstA412>
2019-03-04 12:29:44 +0100

    chat dans la custodies view

--- app/controllers/custodies_controller.rb
+++ app/controllers/custodies_controller.rb
@@ -1,17 +1,24 @@
 class CustodiesController < ApplicationController
+  before_action :authenticate_user!
   before_action :set_custodies, only: %i[show edit update]
 
   def index
     params[:number] ||= 0
     @lundicourant = Date.parse("Monday").to_time
-    @startdate = @lundicourant + (7 * params[:number].to_i).day
-    @enddate = @startdate + 6.day
-    @custodies = Custody.where(day_on: @startdate..@enddate)
+    @startdate    = @lundicourant + (7 * params[:number].to_i).day
+    @enddate      = @startdate + 6.day
+    @custodies    = Custody.where(day_on: @startdate..@enddate)
+
+    @sender   = current_user
+    @receiver = User.where(child_id: current_user.child_id).where.not(id: current_user.id).first
+    p "rediriger vers 'inviter l'autre parent" if @receiver.nil?
+    @message  = Message.new
+    @messages = Message.all
   end
 
   def show
     @custody = Custody.find(params[:id])
-    @media = @custody.media
+    @media   = @custody.media
   end
 
   def new
@@ -24,6 +31,14 @@ class CustodiesController < ApplicationController
       redirect_to root_path
     else
       render :new
+    end
+
+    @message  = Message.new(message_params)
+    @user     = User.find(params[:message][:receiver_id])
+    if @message.save!
+      redirect_to messages_path
+    else
+      @message
     end
   end
 
@@ -46,4 +61,7 @@ class CustodiesController < ApplicationController
     params.require(:custody).permit(:title, :text, :day_on, :user)
   end
 
+  def message_params
+    params.require(:message).permit(:body, :sender_id, :receiver_id)
+  end
 end
