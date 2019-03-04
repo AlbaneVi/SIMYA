cee94fb41c459e2bd25c369ada1bb80889cbd525
emna <emna.dridi29@gmail.com>
2019-03-04 10:54:51 +0100

    user controller created

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,18 @@
+class UsersController < ApplicationController
+  before_action :authenticate_user!
+  before_action :set_users, only: [:show, :edit]
+
+  def show
+    # set_users
+  end
+
+  def edit
+    # set_users
+  end
+
+  private
+
+  def set_users
+    @user = User.find(params[:id])
+  end
+end
