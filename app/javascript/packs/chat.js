66e36fd3e3de44bc0d0e1505884ade6abb6283e7
ahkoon <gAngstA412>
2019-03-05 12:37:37 +0100

    pop up chat js

--- app/javascript/packs/chat.js
+++ app/javascript/packs/chat.js
@@ -1,10 +1,14 @@
 btn = document.querySelector(".button-open-chat");
 chat = document.querySelector(".message");
+messages = document.querySelector(".message-user");
 
 btn.addEventListener("onClick", (event) => {
     chat.style.display ="";
-  });
+});
 
 btn.addEventListener("click", (event) => {
     chat.classList.toggle("display");
-  });
+    // put scroll messages to bottom
+    messages.scrollTop = messages.scrollollHeight - messages.clientHeight;
+});
+
