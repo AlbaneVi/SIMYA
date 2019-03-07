d92063c1e49e4fd6890b9ca533870b501c23bbc3
ahkoon <gAngstA412>
2019-03-07 11:06:27 +0100

    sender receiver problem - fixed

--- app/javascript/packs/chat.js
+++ app/javascript/packs/chat.js
@@ -2,6 +2,7 @@
 const btn = document.querySelector(".button-open-chat");
 const chat = document.querySelector(".message");
 const close = document.querySelector(".btn-close-chat");
+// const open = docuement.querySelector(".message");
 
 
 const initChat = () => {
@@ -13,16 +14,26 @@ const initChat = () => {
     chat.style.display ="";
 });
 
+function scrollToBottom() {
+  const objDiv = document.querySelector(".message-user");
+  objDiv.scrollTop = objDiv.scrollHeight;
+}
+
   btn.addEventListener("click", (event) => {
     chat.classList.toggle("display");
     btn.style.display = "none";
-
+    scrollToBottom();
   });
 
   close.addEventListener("click", (event) => {
     chat.classList.toggle("display");
     btn.style.display = "";
   })
+
+  // open.addEventListener("message", (event) => {
+  //   chat.classList.toggle("display");
+  //   btn.style.display = "";
+  // }
 
 }
 
