1bd818a6d31e10f5949c018c7fc6c7f7019db7ef
AlbaneVi <albane.villard@gmail.com>
2019-03-07 10:31:15 +0100

    seeds again

--- db/seeds.rb
+++ db/seeds.rb
@@ -76,8 +76,8 @@ Custody.create!(day_on: Date.today - 8,
 #seeds 01/03
 Custody.create!(day_on: Date.today - 7,
                 user: c1_parent2,
-                title: title,
-                text: text)
+                title: nil,
+                text: nil)
 
 #seeds 02/03
 Custody.create!(day_on: Date.today - 6,
@@ -118,145 +118,145 @@ Custody.create!(day_on: Date.today - 1,
 #seeds 08/03
 Custody.create!(day_on: Date.today,
                 user: c1_parent1,
-                title: title,
-                text: text)
+                title: nil,
+                text: nil)
 
 #seeds 09/03
 Custody.create!(day_on: Date.today + 1,
                 user: c1_parent2,
-                title: title,
-                text: text)
+                title: nil,
+                text: nil)
 
 #seeds 10/03
 Custody.create!(day_on: Date.today + 2,
                 user: c1_parent2,
-                title: title,
-                text: text)
+                title: nil,
+                text: nil)
 
 #seeds lundi 11/03
 Custody.create!(day_on: Date.today + 3,
                 user: c1_parent1,
-                title: title,
-                text: text)
+                title: nil,
+                text: nil)
 
 #seeds 12/03
 Custody.create!(day_on: Date.today + 4,
                 user: c1_parent1,
-                title: title,
-                text: text)
+                title: nil,
+                text: nil)
 
 #seeds 13/03
 Custody.create!(day_on: Date.today + 5,
                 user: c1_parent2,
-                title: title,
-                text: text)
+                title: nil,
+                text: nil)
 
 #seeds 14/03
 Custody.create!(day_on: Date.today + 6,
                 user: c1_parent2,
-                title: title,
-                text: text)
+                title: nil,
+                text: nil)
 
 #seeds 15/03
 Custody.create!(day_on: Date.today + 7,
                 user: c1_parent2,
-                title: title,
-                text: text)
+                title: nil,
+                text: nil)
 
 #seeds 16/03
 Custody.create!(day_on: Date.today + 8,
                 user: c1_parent2,
-                title: title,
-                text: text)
+                title: nil,
+                text: nil)
 
 #seeds 17/03
 Custody.create!(day_on: Date.today + 9,
                 user: c1_parent2,
-                title: title,
-                text: text)
+                title: nil,
+                text: nil)
 
 #seeds lundi 18/03
 Custody.create!(day_on: Date.today + 10,
                 user: c1_parent1,
-                title: title,
-                text: text)
+                title: nil,
+                text: nil)
 
-#seeds 19/03
+#seeds 19/03nil
 Custody.create!(day_on: Date.today + 11,
                 user: c1_parent1,
-                title: title,
-                text: text)
+                title: nil,
+                text: nil)
 
 #seeds 20/03
 Custody.create!(day_on: Date.today + 12,
                 user: c1_parent2,
-                title: title,
-                text: text)
+                title: nil,
+                text: nil)
 
 #seeds 21/03
 Custody.create!(day_on: Date.today + 13,
                 user: c1_parent2,
-                title: title,
-                text: text)
+                title: nil,
+                text: nil)
 
 #seeds 22/03
 Custody.create!(day_on: Date.today + 14,
                 user: c1_parent1,
-                title: title,
-                text: text)
+                title: nil,
+                text: nil)
 
 #seeds 23/03
 Custody.create!(day_on: Date.today + 15,
                 user: c1_parent1,
-                title: title,
-                text: text)
+                title: nil,
+                text: nil)
 
 #seeds 24/03
 Custody.create!(day_on: Date.today + 16,
                 user: c1_parent1,
-                title: title,
-                text: text)
+                title: nil,
+                text: nil)
 
 #seeds lundi 25/03
 Custody.create!(day_on: Date.today + 17,
                 user: c1_parent1,
-                title: title,
-                text: text)
+                title: nil,
+                text: nil)
 
 #seeds 26/03
 Custody.create!(day_on: Date.today + 18,
                 user: c1_parent1,
-                title: title,
-                text: text)
+                title: nil,
+                text: nil)
 
 #seeds 27/03
 Custody.create!(day_on: Date.today + 19,
                 user: c1_parent2,
-                title: title,
-                text: text)
+                title: nil,
+                text: nil)
 
 #seeds 28/03
 Custody.create!(day_on: Date.today + 20,
                 user: c1_parent2,
-                title: title,
-                text: text)
+                title: nil,
+                text: nil)
 
 #seeds 29/03
 Custody.create!(day_on: Date.today + 21,
                 user: c1_parent2,
-                title: title,
-                text: text)
+                title: nil,
+                text: nil)
 
 #seeds 30/03
 Custody.create!(day_on: Date.today + 22,
                 user: c1_parent2,
-                title: title,
-                text: text)
+                title: nil,
+                text: nil)
 
 #seeds 31/03
 Custody.create!(day_on: Date.today + 23,
                 user: c1_parent2,
-                title: title,
-                text: text)
+                title: nil,
+                text: nil)
 
 
